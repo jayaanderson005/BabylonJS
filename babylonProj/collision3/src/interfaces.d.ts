08c7c2ee4ed01d9b56462c0826d0e76597e407c9
jayaanderson005 <jay.a.anderson.005@gmail.com>
2025-11-13 16:52:29 +0000

    collisions
    the build doesn't work

---
+++ babylonProj/collision3/src/interfaces.d.ts
@@ -0,0 +1,19 @@
+import {
+  Scene,
+  Sound,
+  Mesh,
+  HemisphericLight,
+  Camera,
+  ISceneLoaderAsyncResult,
+} from "@babylonjs/core";
+
+export interface SceneData {
+  scene: Scene;
+  //audio: Sound;
+  lightHemispheric: HemisphericLight;
+  camera: Camera;
+  box1: Mesh;
+  box2: Mesh;
+  player: Promise<void | ISceneLoaderAsyncResult>;
+  ground: Mesh;
+}
